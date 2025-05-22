2b484e97f9356844e8e9fc4c9995f7535bb10b18
seonghun120614 <seonghun120614@gmail.com>
2025-05-22 19:20:48 +0900

    chore: configure log4j2 for custom logging and  for initial code execution before application start

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -1,9 +1,9 @@
 package com.example.study;
 
 import com.example.study.config.*;
+import com.example.study.events.*;
 import com.example.study.service.*;
 import org.slf4j.*;
-import org.slf4j.Logger;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.*;
@@ -21,14 +21,18 @@ import org.springframework.core.env.Environment;
 //@EnabledConfigurationProperties("AppProperties.class") - 삭제된 annotation
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-public class StudyApplication {
+public class StudyApplication
+		//implements CommandLineRunner
+{
 
 	/*
 	 Spring Logger Facade for Java 의 라이브러리에서
 	 각종 Logback, Log4j, Log4j2, java.util.logging, tinylog
-	 의 라이브러리를 종합하여 간소화한 형태의 interface 로 제공해줌
+	 의 라이브러리를 종합하여 간소화한 형태의 interface 로 제공해줌.
+
+	 지금 이 클래스가 가지고 있는 Logger 를 가져온다는 뜻
 	 */
-	private static final Logger log =
+	private static final Logger logger =
 			LoggerFactory.getLogger(StudyApplication.class);
 
 	/* run()
@@ -42,37 +46,54 @@ public class StudyApplication {
 	 */
 	public static void main(String[] args) {
 
-		ConfigurableApplicationContext applicationContext =
-				SpringApplication.run(StudyApplication.class, args);
-		Environment env = applicationContext.getBean(Environment.class);
+		SpringApplication springApplication
+				= new SpringApplication(StudyApplication.class);
 
-		// app.timeout 의 value 를 읽는데 해당 value 는 환경설정으로 설정됨
-//		log.info("Timeout : " + env.getProperty("app.timeout"));
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+
+		/*
+		 addListeners 는 가변인자를 받기 때문에 여러개 등록 가능
+		 이 방식은 SpringApplication 의 소스코드 변경을 유발하기에
+		 Spring.factories 프로퍼티 파일을 사용해 커스텀 리스너 추가 가능
+		 */
+		springApplication.addListeners(
+				new ApplicationContextInitializedEventListener(),
+				new ApplicationEnvironmentPreparedEventListener(),
+				new ApplicationFailedEventListener(), // 실행 안되는게 맞음
+				new ApplicationPreparedEventListener(),
+				new ApplicationReadyEventListener(),
+				new ApplicationStartedEventListener(),
+				new ApplicationStartingEventListener(), // Logging 이 안보이는게 맞음
+				new ContextRefreshedEventListener(),
+				new WebServerInitializedEventListener() // 웹서버는 아니라서 logging 안보임
+		);
+
+		ConfigurableApplicationContext applicationContext =
+				springApplication.run(args);
+
+		Environment env = applicationContext.getBean(Environment.class);
+		// 값 읽는 방법 - log.info("Timeout : " + env.getProperty("app.timeout"));
 
 		DbConfiguration dbConfiguration = applicationContext.getBean(DbConfiguration.class);
 
-		// EnableConfigurationProperties 를 사용하면 선언한 Properties 들을 Bean 으로 등록
 		AppService appService = applicationContext.getBean(AppService.class);
-		log.info(appService.getAppProperties().toString());
+
+//		logger.info(appService.getAppProperties().toString());
+//		logger.info(dbConfiguration.toString());
+//		logger.info(env.getProperty("user"));
 
 		/*
 		각 프로퍼티들이 전부 모여 Environment 객체에 로딩됨.
 		따라서 어플리케이션 클래스에서 Environment 인스턴스에
 		접근해서 설정 정보를 읽을 수 있으며 @Value 애너테이션을
 		통해 접근 가능
-
 		프로퍼티를 읽어들이는 순위는
 		1. SpringApplication
 		2. PropertySource
 		3. 설정 정보 파일(application.properties)
 		4. 운영 체제 환경 변수
 		5. 명령행 인자
-		 */
-		log.info("\n\n");
-		log.info(dbConfiguration.toString());
-		log.info(env.getProperty("user"));
 
-		/*
 		 Properties 를 소스코드로도 설정가능 함.
 		 이는 설정 정보가 나중에 바뀌지 않는 경우 사용할 때 적합하다.
 		 즉, dev, test, prod 와 같은 properties 를 만들어 놓으면 좋을 것이다.
@@ -82,35 +103,43 @@ public class StudyApplication {
 		 setProperty 를 사용해 String, Object 로 property 를 넣음
 		 properties.setProperty("spring.config.import", "additional-application.properties");
 		 properties.setProperty("spring.config.on-not-found", "ignore");
-		 */
 
-//		SpringApplication springApplication =
-//				new SpringApplication(StudyApplication.class);
-		// springApplication.setWebApplicationType(WebApplicationType.SERVLET);
-
-		/*
-		 * 설정한 Properties 넣음, 보통 addListeners 보다 먼저 호출
-		 * Listeners 가 properties 를 의존 할 수도 있기 때문
-		 * springApplication.setDefaultProperties(properties);
+		 설정한 Properties 넣음, 보통 addListeners 보다 먼저 호출
+		 Listeners 가 properties 를 의존 할 수도 있기 때문
+		 springApplication.setDefaultProperties(properties);
 		 */
-
-		/*
-		 * addListeners 는 가변인자를 받기 때문에 여러개 등록 가능
-		 * 이 방식은 SpringApplication 의 소스코드 변경을 유발하기에
-		 * Spring.factories 프로퍼티 파일을 사용해 커스텀 리스너 추가 가능
-		 */
-//		springApplication.addListeners(
-//				new ApplicationContextInitializedEventListener(),
-//				new ApplicationEnvironmentPreparedEventListener(),
-//				new ApplicationFailedEventListener(),
-//				new ApplicationPreparedEventListener(),
-//				new ApplicationReadyEventListener(),
-//				new ApplicationStartedEventListener(),
-//				new ApplicationStartingEventListener(),
-//				new ContextRefreshedEventListener(),
-//				new WebServerInitializedEventListener()
-//		);
 	}
+
+	/*
+	CommandLineRunner 를 통해 ApplicationStartedEvent 의 발행
+	직후에 실행되는 메서드, CommandLineRunner 를 implements 해야함.
+	 */
+
+	// 1번째 방법 - 제한적임. 오로지 하나의 메서드에서만 실행가능.
+//	@Override
+//	public void run(String... args) throws Exception {
+//		logger.info("StudyApplication CommandLineRunner has executed");
+//	}
+
+	/*
+	 2번째 방법
+	 Bean 에 등록해놓으면 알아서 CommandLineRunner 를 필요로 하는(인자로 취하는)
+	 메서드에 주입을 자동으로 하기 때문에 이런 방법이 가능할거라 생각.
+	 또한, Bean 으로 등록됐기 때문에 인자로 다른 Bean 으로 등록된 객체를 넣을 수 있음
+	 */
+//	@Bean
+//	public CommandLineRunner commandLineRunner() {
+//		return args -> {
+//			logger.info("CommandLineRunner executed as a bean definition with" + args.length + " arguments.");
+//			for (String arg : args) {
+//				logger.info("Argument: " + arg);
+//			}
+//		};
+//	}
+
+	/*
+	3번째 방법은 다른 클래스를 참고하여야 한다.
+	 */
 
 //  이는 초기 event 의 감지는 잘 못하는 경향이 있음 따라서 위와 같이 리스너를 등록하기
 //	@EventListener(ApplicationReadyEvent.class)
--- src/main/java/com/example/study/events/AbstractLoggingListener.java
+++ src/main/java/com/example/study/events/AbstractLoggingListener.java
@@ -1,14 +1,16 @@
 package com.example.study.events;
 
-import com.example.study.log.*;
+import org.slf4j.*;
 import org.springframework.context.*;
 
 public abstract class AbstractLoggingListener<T extends ApplicationEvent>
         implements ApplicationListener<T> {
+
     protected abstract String message();
 
     // 이벤트가 publish 되면 실행됨.
     public void onApplicationEvent(T event) {
-        Logger.logging(event, message());
+        Logger logger = LoggerFactory.getLogger(getClass());
+        logger.warn(event + " - " + message());
     }
 }
